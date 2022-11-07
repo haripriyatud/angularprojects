70eeb57a1b102036e351ec359a96f5cf0452629e
haripriyatud <haripriyasubramanian@gmail.com>
2022-11-07 08:34:08 +0100

    home page modified

--- websiteexample/src/app/app.module.ts
+++ websiteexample/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
